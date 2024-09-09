90945e92a7441340a3d38abd448115a1287b0265
leedhfsd <leedhfsd@gmail.com>
2024-09-09 15:07:40 +0900

    [feat] (이대현) 스프링 초기 환경 설정

--- back/payment/src/main/java/com/moa/payment/PaymentApplication.java
+++ back/payment/src/main/java/com/moa/payment/PaymentApplication.java
@@ -2,8 +2,9 @@ package com.moa.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class PaymentApplication {
 
 	public static void main(String[] args) {
