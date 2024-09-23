86598e04b648202c9d89ffbd9bd20c7cf56d342e
Kim Seongeun <cellnews0514@gmail.com>
2024-09-23 17:41:49 +0900

    [feat] (김성은) 참여자쪽 더치페이 단계별 설정 및 화면 전환

---
+++ front/src/components/dutch/Payment/Payment.tsx
@@ -0,0 +1,16 @@
+import Product from "../Product/Product";
+import SquareBtn from "../SquareBtn/SquareBtn";
+
+import {
+  Wrapper,
+} from "./Payment.styles"
+
+const Payment = () => {
+  return (
+    <Wrapper>
+
+    </Wrapper>
+  )
+}
+
+export default Payment;
