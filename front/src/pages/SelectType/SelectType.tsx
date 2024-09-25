d39aecac3f3200ab798e5dac6f08f7e4f3bb2bdb
dpqls0356 <dpqls0356@gmail.com>
2024-09-26 02:59:22 +0900

    [feat] (이예빈) 혜택/실적 선택 기능 구현

--- front/src/pages/SelectType/SelectType.tsx
+++ front/src/pages/SelectType/SelectType.tsx
@@ -1,5 +1,51 @@
+import { useEffect, useState } from "react";
+import { SelectView, Story, Wrapper } from "./SelectType.styles";
+import axios, { AxiosError } from "axios";
+import { useNavigate } from "react-router-dom";
+import { PATH } from "../../constants/path";
+import { useAuthStore } from "../../store/AuthStore";
+
 const SelectType = () => {
-  return <></>;
+  const navigate = useNavigate();
+  const { isLoggedIn, Login } = useAuthStore;
+  const [isLoading, setIsLoading] = useState(true);
+  const SettingType = (type: string) => {
+    //요청보내기
+    try {
+      axios.post(``, { type }, { withCredentials: true });
+      navigate(PATH.HOME);
+    } catch (e) {
+      const error = e as AxiosError;
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    if (!isLoggedIn) {
+      Login();
+    }
+  }, []);
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Story></Story>
+      ) : (
+        <SelectView>
+          <div
+            onClick={() => {
+              SettingType("benefit");
+            }}>
+            혜택형
+          </div>
+          <div
+            onClick={() => {
+              SettingType("perform");
+            }}>
+            실적형
+          </div>
+        </SelectView>
+      )}
+    </Wrapper>
+  );
 };
 export default SelectType;
 
