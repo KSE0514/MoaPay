5cae2b8753cfb58e4412b5c7c834465eaa7d03ca
leedhfsd <leedhfsd@gmail.com>
2024-09-26 17:33:30 +0900

    [feat] (이대현) docker-compose 수정

--- back/store/src/main/java/com/moa/store/domain/itemInfo/model/ItemInfo.java
+++ back/store/src/main/java/com/moa/store/domain/itemInfo/model/ItemInfo.java
@@ -1,11 +1,14 @@
 package com.moa.store.domain.itemInfo.model;
 
+import java.util.UUID;
+
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicInsert;
 
 import com.moa.store.domain.order.model.Order;
 import com.moa.store.domain.product.model.Product;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -14,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "item_info")
@@ -24,16 +28,24 @@ public class ItemInfo {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
+	@NotNull
+	@Column(columnDefinition = "binary(16)", unique = true, nullable = false, updatable = false)
+	private UUID uuid;
+
+	@NotNull
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "order_id")
 	private Order order;
 
+	@NotNull
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "item_id")
 	private Product item;
 
+	@NotNull
 	@ColumnDefault("1")
 	private Long quantity;
 
+	@NotNull
 	private long price;
 }
