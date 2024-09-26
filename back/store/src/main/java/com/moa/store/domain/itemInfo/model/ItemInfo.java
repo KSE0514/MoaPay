764a052d17cd02e12d9ac13686bf45573294d23e
leedhfsd <leedhfsd@gmail.com>
2024-09-26 16:50:45 +0900

    [feat] (이대현) item_info 엔티티 작성

--- back/store/src/main/java/com/moa/store/domain/itemInfo/model/ItemInfo.java
+++ back/store/src/main/java/com/moa/store/domain/itemInfo/model/ItemInfo.java
@@ -1,4 +1,39 @@
 package com.moa.store.domain.itemInfo.model;
 
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+
+import com.moa.store.domain.order.model.Order;
+import com.moa.store.domain.product.model.Product;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "item_info")
+@DynamicInsert
 public class ItemInfo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "order_id")
+	private Order order;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "item_id")
+	private Product item;
+
+	@ColumnDefault("1")
+	private Long quantity;
+
+	private long price;
 }
