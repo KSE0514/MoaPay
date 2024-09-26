d2b820c9e32262690dcb410372486af5095c9940
이예빈 <dpqls0356@gmail.com>
2024-09-26 11:04:38 +0900

    [chore] (이예빈) cors 설정 수정

--- back/member/src/main/java/com/moa/member/global/config/WebConfig.java
+++ back/member/src/main/java/com/moa/member/global/config/WebConfig.java
@@ -8,7 +8,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-				.allowedOrigins("https://moapay-7e24e.web.app", "http://localhost:18040") // 실제 도메인과 로컬 도메인 모두 허용
+				.allowedOriginPatterns("*") // “*“같은 와일드카드를 사용
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
 				.allowedHeaders("*")
 				.allowCredentials(true); // 쿠키 인증 요청 허용
