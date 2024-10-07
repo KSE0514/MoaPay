303b02bea025ef0850f063caace2999652408bc8
JsooAh <joo1798@naver.com>
2024-10-07 16:35:12 +0900

    [fix] (주수아) 결제 내역 로딩 API에서 날짜 형식이 틀린 경우 그에 맞는 오류 응답을 전송하도록 변경

--- back/moapay/src/main/java/com/moa/moapay/global/exception/GlobalExceptionHandler.java
+++ back/moapay/src/main/java/com/moa/moapay/global/exception/GlobalExceptionHandler.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.threeten.bp.DateTimeException;
+import java.time.DateTimeException;
 
 @Slf4j
 @RestControllerAdvice
