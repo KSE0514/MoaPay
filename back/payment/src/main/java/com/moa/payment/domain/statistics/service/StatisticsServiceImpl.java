f3e0832efbfb5aea29c288a8182f83d6e25a4334
JsooAh <joo1798@naver.com>
2024-10-09 19:11:48 +0900

    [chore] (주수아) 로그출력 설정

--- back/payment/src/main/java/com/moa/payment/domain/statistics/service/StatisticsServiceImpl.java
+++ back/payment/src/main/java/com/moa/payment/domain/statistics/service/StatisticsServiceImpl.java
@@ -42,8 +42,14 @@ public class StatisticsServiceImpl implements StatisticsService {
         int lastDay = dateInfo.atEndOfMonth().lengthOfMonth();
         LocalDateTime startTime = LocalDateTime.of(year, month, 1, 0, 0);
         LocalDateTime endTime = LocalDateTime.of(year, month, lastDay, 23, 59);
-        return paymentLogQueryRepository.findAllCardsPaymentLogs(cardIds,
+        List<CardHistoryPaymentLogDto> result = paymentLogQueryRepository.findAllCardsPaymentLogs(cardIds,
             startTime, endTime);
+        log.info("getPaymentLogs -> result count : {}", result.size());
+        for(CardHistoryPaymentLogDto logDto : result) {
+            log.info(logDto.toString());
+        }
+        log.info("log list ended");
+        return result;
     }
 
     public List<UUID> getCardsUUID(GetMyCardIdsRequestDto getMyCardIdsRequestDto) {
