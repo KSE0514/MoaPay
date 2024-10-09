c0814da9964d0da5e16a094d0a7e94eb242c1d8f
이예빈 <dpqls0356@gmail.com>
2024-10-09 23:18:35 +0900

    [chore] (이예빈) 시큐리티 설정 수정

--- back/gateway/src/main/java/com/moa/gateway/global/config/SecurityConfig.java
+++ back/gateway/src/main/java/com/moa/gateway/global/config/SecurityConfig.java
@@ -89,23 +89,23 @@ public class SecurityConfig {
                 ).addFilterBefore(authenticationWebFilter(), SecurityWebFiltersOrder.AUTHENTICATION).build();
     }
 
-    // CorsConfigurationSource 메서드 (SecurityConfig 클래스 내부에 추가)
-    @Bean
-    public org.springframework.web.cors.reactive.CorsConfigurationSource corsConfigurationSource() {
-        org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource source =
-                new org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList(
-                "https://localhost:8765", "http://localhost:8765", "https://localhost", "http://localhost",
-                "http://localhost:5173", "https://localhost:5173", "https://moapay-7e24e.web.app",
-                "https://j11c201.p.ssafy.io", "https://j11c201.p.ssafy.io/api", "http://j11c201.p.ssafy.io"
-        ));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-        config.setAllowedHeaders(Arrays.asList("*"));
-        config.setAllowCredentials(true);
-        config.setMaxAge(3600L);
-        source.registerCorsConfiguration("/**", config);
-        return source;
-    }
+//    // CorsConfigurationSource 메서드 (SecurityConfig 클래스 내부에 추가)
+//    @Bean
+//    public org.springframework.web.cors.reactive.CorsConfigurationSource corsConfigurationSource() {
+//        org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource source =
+//                new org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowedOrigins(Arrays.asList(
+//                "https://localhost:8765", "http://localhost:8765", "https://localhost", "http://localhost",
+//                "http://localhost:5173", "https://localhost:5173", "https://moapay-7e24e.web.app",
+//                "https://j11c201.p.ssafy.io", "https://j11c201.p.ssafy.io/api", "http://j11c201.p.ssafy.io"
+//        ));
+//        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+//        config.setAllowedHeaders(Arrays.asList("*"));
+//        config.setAllowCredentials(true);
+//        config.setMaxAge(3600L);
+//        source.registerCorsConfiguration("/**", config);
+//        return source;
+//    }
 
 }
