746cf2e3d73da3781d32856f6a8175b7b1139d14
‘leedhfsd’ <leedhfsd@gmail.com>
2024-10-09 15:53:36 +0900

    [fix] (이대현) kafka 설정 변경

--- back/payment/src/main/java/com/moa/payment/domain/statistics/service/MoaPayClient.java
+++ back/payment/src/main/java/com/moa/payment/domain/statistics/service/MoaPayClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.moa.payment.domain.statistics.model.dto.GetMyCardIdsRequestDto;
 import com.moa.payment.global.response.ResultResponse;
 
-@FeignClient(name = "moapay", url = "https://j11c201.p.ssafy.io")
+@FeignClient(name = "moapay", url = "https://j11c201.p.ssafy.io/api")
 public interface MoaPayClient {
 
     @PostMapping("core/card/getMyCardIds")
--- back/store/src/main/java/com/moa/store/domain/paymentInfo/client/MoaPayClient.java
+++ back/store/src/main/java/com/moa/store/domain/paymentInfo/client/MoaPayClient.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "moapay", url = "https://j11c201.p.ssafy.io")
+@FeignClient(name = "moapay", url = "https://j11c201.p.ssafy.io/api")
 public interface MoaPayClient {
 	@PostMapping("moapay/core/code/QRcode")
 	ResponseEntity<ResultResponse> getQRCode(@RequestBody CreateOrderResponseDto createOrderResponseDto);
