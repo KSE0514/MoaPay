8806a54779e5e22f4dd6c791d382e5ec94934253
Kim Seongeun <cellnews0514@gmail.com>
2024-10-15 01:40:55 +0900

    [design] (김성은) 더치페이 css 수정중

--- front/src/pages/Dutchpay/Dutchpay.tsx
+++ front/src/pages/Dutchpay/Dutchpay.tsx
@@ -895,7 +895,7 @@ const Dutchpay = () => {
                 </Bottom>
               </DutchFin>
             ) : null}
-            //TODO: 여기도 바꿔야함
+            {/* //TODO: 여기도 바꿔야함 */}
             {process === 6 ? (
               <DutchComplite roomId={roomId}></DutchComplite>
             ) : null}
